0ce27bf7c111f7efa1f82a746c91d361dacb87e3
Niklas Fulle <36989748+Salkin263@users.noreply.github.com>
2025-01-01 23:56:02 +0100

    added comments to the terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,46 +1,56 @@
+# Configure the Virtual Private Cloud (VPC)
 resource "aws_vpc" "my_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_dns_support  = true
 
   tags = {
+    # Name tag for the VPC
     Name = "ec2_vpc"
   }
 }
 
+# Configure a Public Subnet within the VPC
 resource "aws_subnet" "my_public_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  vpc_id               = aws_vpc.my_vpc.id
+  cidr_block           = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "eu-central-1a"
+  availability_zone    = "eu-central-1a"
 
   tags = {
+    # Name tag for the public subnet
     Name = "dev-public"
   }
 }
 
+# Configure the Internet Gateway for the VPC
 resource "aws_internet_gateway" "my_internet_gateway" {
   vpc_id = aws_vpc.my_vpc.id
 
   tags = {
+    # Name tag for the internet gateway
     Name = "dev-igw"
   }
 }
 
+# Configure the Public Route Table for the VPC
 resource "aws_route_table" "my_public_rt" {
   vpc_id = aws_vpc.my_vpc.id
 
   tags = {
+    # Name tag for the public route table
     Name = "dev_public_rt"
   }
 }
 
+# Configure a Default Route in the Public Route Table
 resource "aws_route" "default_route" {
   route_table_id         = aws_route_table.my_public_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.my_internet_gateway.id
 }
 
+# Associate the Public Subnet with the Public Route Table
 resource "aws_route_table_association" "my_public_assoc" {
   subnet_id      = aws_subnet.my_public_subnet.id
   route_table_id = aws_route_table.my_public_rt.id
@@ -48,7 +58,6 @@ resource "aws_route_table_association" "my_public_assoc" {
 
 
 # Create EC2 Security Group and Security Rules
-
 resource "aws_security_group" "my_sg" {
   name        = "dev_sg"
   description = "dev security group"
@@ -70,7 +79,6 @@ resource "aws_security_group" "my_sg" {
 
 
 # Create SSH Keys for EC2 Remote Access
-
 resource "tls_private_key" "public_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -89,7 +97,6 @@ resource "aws_key_pair" "web_tier_EC2_rsa_key" {
 
 
 # Create EC2 Instance
-
 resource "aws_instance" "ec2_dev" {
   instance_type          = "t2.micro"
   ami                    = data.aws_ami.server_ami.id
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,12 +1,18 @@
+# This block defines the required Terraform providers for this configuration
 terraform {
   required_providers {
+    # defines the AWS provider
     aws = {
+      # This argument specifies the source of the AWS provider plugin
       source  = "hashicorp/aws"
+      # This argument specifies the required version constraint for the AWS provider
       version = "~> 5.0"
     }
   }
 }
 
+# configures the AWS provider
 provider "aws" {
+  # specifies the AWS region to use
   region = "eu-central-1"
 }
